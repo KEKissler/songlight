a0473ceaf0e2a4d71c7249ee76f86f2ecbd728cb
Paperboy64 <33039142+KEKissler@users.noreply.github.com>
2018-09-04 03:02:43 -0700

    Initial Project Setup

---
+++ Assets/Scripts/EmitterController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class EmitterController : MonoBehaviour {
+    public float heldVolumePercentage;
+    public Transform emitterParent;
+
+    private AudioSource aud;
+
+	void Start () {
+        aud = GetComponent<AudioSource>();
+        heldVolumePercentage = Mathf.Clamp(heldVolumePercentage, 0, 1);
+	}
+
+    public void PickUp(Transform source)
+    {
+        transform.parent = source;
+        transform.position = source.transform.position;
+        aud.volume *= heldVolumePercentage;
+    }
+
+    public void PutDown()
+    {
+        transform.parent = emitterParent;
+        aud.volume /= heldVolumePercentage;
+    }
+}
---
+++ Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovementController : MonoBehaviour {
+    public float speed = 1.0f;
+    public KeyCode pickUpEmitterKey;
+
+    private List<GameObject> nearbyObjects = new List<GameObject>();
+    private Rigidbody rb;
+    private GameObject heldEmitter = null;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update () {
+        rb.velocity = speed * new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if (Input.GetKeyDown(pickUpEmitterKey))
+        {
+            if(heldEmitter)
+            {
+                heldEmitter.GetComponent<EmitterController>().PutDown();
+                heldEmitter = null;
+            }
+            else
+            {
+                heldEmitter = pickUpNearestEmitter(nearbyObjects);
+                if(heldEmitter)
+                heldEmitter.GetComponent<EmitterController>().PickUp(this.transform);
+            }
+            
+        }
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        nearbyObjects.Add(other.gameObject);
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        nearbyObjects.Remove(other.gameObject);
+    }
+
+    private GameObject pickUpNearestEmitter(List<GameObject> emitters)
+    {
+        GameObject toReturn = null;
+        float minDistance = float.MaxValue;
+        foreach(GameObject g in emitters)
+        {
+            if(g && g.GetComponent<EmitterController>() && Vector3.Distance(transform.position, g.transform.position) < minDistance)
+            {
+                toReturn = g;
+                minDistance = Vector3.Distance(transform.position, g.transform.position);
+            }
+        }
+        return toReturn;
+    }
+}
